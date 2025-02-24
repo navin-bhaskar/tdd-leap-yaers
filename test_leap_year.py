87d7531c84f5770b7563a02c6f8d0490ed677aa8
Navin Bhaskar <navin.bhaskar.5@gmail.com>
2025-02-24 13:19:03 +0530

    Add UT for second req

--- test_leap_year.py
+++ test_leap_year.py
@@ -7,3 +7,10 @@ class TestLeapYear(unittest.TestCase):
         """All years divisible by 400 ARE leap years (so, for example, 2000 was indeed a leap year)"""
         self.assertEqual(is_leap_year(2000), True)
 
+    def test__all_years_divisible_by_100_but_not_by_400__is_leap_years_returns_false(self):
+        """All years divisible by 100 but not by 400 are NOT leap years 
+        (so, for example, 1700, 1800, and 1900 were NOT leap years, NOR will 2100 be a leap year),"""
+        self.assertEqual(is_leap_year(1700), False)
+        self.assertEqual(is_leap_year(1800), False)
+        self.assertEqual(is_leap_year(1900), False)
+

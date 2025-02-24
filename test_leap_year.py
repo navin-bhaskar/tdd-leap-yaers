f8c3d2ddda7647eecbfcd5da4a0c722fba2c98bb
Navin Bhaskar <navin.bhaskar.5@gmail.com>
2025-02-24 13:15:38 +0530

    Add UT for first req: All year devisible by 4 is leap year

--- test_leap_year.py
+++ test_leap_year.py
@@ -2,3 +2,8 @@ import unittest
 
 from leap_year_checker import is_leap_year
 
+class TestLeapYear(unittest.TestCase):
+    def test__all_years_divisible_by_400__is_leap_year_returns_true(self):
+        """All years divisible by 400 ARE leap years (so, for example, 2000 was indeed a leap year)"""
+        self.assertEqual(is_leap_year(2000), True)
+
